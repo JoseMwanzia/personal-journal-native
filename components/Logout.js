b242b6b6ed7e542ff93eb8c9fcaf10df45bc551b
joseph mwanzia <makamwanzia@gmail.com>
2024-07-06 23:46:08 +0300

    logout button

---
+++ components/Logout.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import { Alert, SafeAreaView, Text, StyleSheet, Button, TouchableOpacity } from 'react-native'
+
+export default function Logout({ navigation }) {
+
+    async function handelLogout() {
+        const response = await fetch('http://192.168.100.166:3000/logout', {
+            method: "DELETE"
+        })
+        const result = await response.json();
+        result ? (setData(result), navigation.navigate('Login'), Alert.alert('You have successfully logged out!')) : Alert.alert('Logout Failed!')
+    }
+
+    return (
+        <TouchableOpacity style={styles.button} onPress={handelLogout}>
+            <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        paddingTop: 70,
+    },
+    image: {
+        height: 180,
+        width: 200
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        textAlign: "center",
+        paddingVertical: 40,
+        color: "black"
+    },
+    inputView: {
+        gap: 15,
+        width: "100%",
+        paddingHorizontal: 40,
+        marginBottom: 5
+    },
+    input: {
+        height: 50,
+        paddingHorizontal: 20,
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 7
+    },
+    rememberView: {
+        width: "100%",
+        paddingHorizontal: 50,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        marginBottom: 8
+    },
+    switch: {
+        flexDirection: "row",
+        gap: 1,
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    rememberText: {
+        fontSize: 13
+    },
+    forgetText: {
+        fontSize: 11,
+        color: "red"
+    },
+    button: {
+        backgroundColor: "red",
+        height: 45,
+        borderColor: "gray",
+        borderWidth: 1,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    buttonView: {
+        width: "100%",
+        paddingHorizontal: 50
+    },
+    optionsText: {
+        textAlign: "center",
+        paddingVertical: 10,
+        color: "gray",
+        fontSize: 13,
+        marginBottom: 6
+    },
+    mediaIcons: {
+        flexDirection: "row",
+        gap: 15,
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 23
+    },
+    icons: {
+        width: 40,
+        height: 40,
+    },
+    footerText: {
+        textAlign: "center",
+        color: "gray",
+    },
+    signup: {
+        color: "red",
+        fontSize: 15
+    }
+})
