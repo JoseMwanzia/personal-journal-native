d62bc9bea885d296309c449feb8137fbd95b42e4
joseph mwanzia <makamwanzia@gmail.com>
2024-07-09 01:24:46 +0300

    styles

--- components/CreatedJournals.js
+++ components/CreatedJournals.js
@@ -92,3 +92,21 @@ export default ({ userData }) => {
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10,
+    },
+    separator: {
+        height: 1,
+        width: '80%',
+        backgroundColor: 'grey',
+        marginVertical: 10,
+    },
+});
