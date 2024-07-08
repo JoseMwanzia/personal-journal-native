956044e517af01a7e078e4d3168c9e4a81eff351
joseph mwanzia <makamwanzia@gmail.com>
2024-07-09 01:52:33 +0300

    styles

--- components/ChangePassword.js
+++ components/ChangePassword.js
@@ -68,3 +68,28 @@ export default function ChangePassword({ navigation, userData }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 16,
+        paddingHorizontal: 8,
+        width: '100%',
+    },
+    accordion: {
+        marginTop: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 5,
+        width: 250,
+    },
+});
