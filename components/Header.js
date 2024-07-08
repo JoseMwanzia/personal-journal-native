d97c3c1f6ed2a1bdd699094b520e7f870eef91e8
joseph mwanzia <makamwanzia@gmail.com>
2024-07-09 01:35:07 +0300

    navigation bar screen with user settings

---
+++ components/Header.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Header, Icon, Button, Overlay, Avatar } from 'react-native-elements';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Logout from './Logout';
+import ChangeUserName from './ChangeUserName';
+import ChangePassword from './ChangePassword';
+
+
+const App = ({ userData }) => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+
+    return (
+        <View style={{}}>
+            <Header
+                backgroundColor="black"
+                backgroundImageStyle={{}}
+                barStyle="light-content"
+                centerComponent={{
+                    text: "SHAMIRI",
+                    style: { color: "#fff" }
+                }}
+                centerContainerStyle={{}}
+                containerStyle={{ width: 'auto' }}
+                leftComponent={
+                    <Icon
+                        name="menu"
+                        color="#fff"
+                        onPress={toggleOverlay}
+                    />
+                }
+                leftContainerStyle={{}}
+                placement="center"
+                rightContainerStyle={{}}
+                statusBarProps={{}}
+            />
+            <Overlay
+                isVisible={visible}
+                onBackdropPress={toggleOverlay}
+                overlayStyle={styles.overlay}
+            >
+                <View style={styles.menu}>
+                    <Avatar
+                        size={'large'}
+                        source={{
+                            uri:
+                                '../assets/avatar.png'
+                        }}
+                    />
+                    <ChangeUserName userData={userData} />
+                    <ChangePassword userData={userData} />
+                    <View style={styles.separator} />
+                    <Logout />
+                </View>
+            </Overlay>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    overlay: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        width: '80%',
+        padding: 0,
+        backgroundColor: 'white',
+        elevation: 5,
+    },
+    menu: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    separator: {
+        height: 1,
+        width: '80%',
+        backgroundColor: 'grey',
+        marginVertical: 10,
+    },
+});
+
+export default App;
